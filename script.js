5fe4abaab912ecf37834f15a07b97ce0f3d008f7
Gabi Draghici <draghici.gabi89@gmail.com>
2024-03-06 17:17:47 +0000

    Clear button functionality fixed

--- script.js
+++ script.js
@@ -35,12 +35,13 @@ $(document).ready(function () {
 
       // Create, add properties to each element - hour, text input area and save button
       var row = $(`<div>`).addClass(`time-block row`);
-      var hourDiv = $(`<div>`).addClass(`hour col`).text(hour);
-      var textDiv = $(`<textarea>`).addClass(`textarea event-input col`);
-      var saveBtn = $(`<button>`).addClass(`saveBtn col`);
+      var hourDiv = $(`<div>`).addClass(`hour col-1`).text(hour);
+      var textDiv = $(`<textarea>`).addClass(`textarea event-input col-9`);
+      var saveBtn = $(`<button>`).addClass(`saveBtn col-1`).text('Save');
+      var clearBtn = $('<button>').addClass('clearBtn col-1').text('Clear');
 
       // Append elements to each row and all rows to the container
-      row.append(hourDiv, textDiv, saveBtn);
+      row.append(hourDiv, textDiv, saveBtn, clearBtn);
       container.append(row);
     }
     // Call the function that remembers the text after page refresh
@@ -55,19 +56,49 @@ $(document).ready(function () {
 
     $(".saveBtn").on("click", function (event) {
       event.preventDefault();
+      console.log("I am pressing the save button");
+
       // Define variable to store text input for each similar fields 
       var inputText = $(this).siblings(".event-input").val().trim();
+
+      // Identify parent row to use as index in the array when saving the input data
+      var index = $(this).parent().index();
       console.log(inputText)
+
       // Define an array variable that gets the information existent in local storage. If nothing exists, create an empty array.
       var storedInputText = JSON.parse(localStorage.getItem("inputTexts")) || [];
 
-      // Push the existing text into the array
-      storedInputText.push(inputText);
+      // Assign input to the time slot
+      storedInputText[index] = inputText;
 
       // Store input text in local storage
       localStorage.setItem("inputTexts", JSON.stringify(storedInputText));
 
     });
+
+     // DONE: Add a clear button next to save button to clear input text
+
+    $(".clearBtn").on("click", function (event) {
+      event.preventDefault();
+      console.log("I am pressing the button"); //check if the button is working
+
+      // Identify parent row to use as index in the array when deleting the input data
+      var index = $(this).parent().index(); 
+
+      //Get the stored array from local storage
+      var storedInputText = JSON.parse(localStorage.getItem("inputTexts")) || [];  
+
+      //Empty the value for current item in array
+      storedInputText[index] = "";
+      
+      //Reset the array in local storage
+      localStorage.setItem("inputTexts", JSON.stringify(storedInputText)); 
+
+      //Clear the input field
+      $(this).siblings(".event-input").val(""); 
+  
+    });
+
 
     // DONE: Function to call when page refreshes displaying the input text
 
@@ -80,9 +111,7 @@ $(document).ready(function () {
     }
     
   }
-  // TODO: Add a clear button next to save button to clear input text
 
-  
   //Call functions
   updateHeaderDate();
   dailyPlanner();
